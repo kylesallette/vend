8e0cade79fe383fa6043bae5b653994e4db1d811
kylesallette <kylesallette@gmail.com>
2018-02-18 22:01:30 -0700

    final test

--- spec/features/user_visits_snack_page_and_sees_name_price_spec.rb
+++ spec/features/user_visits_snack_page_and_sees_name_price_spec.rb
@@ -10,3 +10,19 @@ describe "User visit snack  page" do
 
   end
 end
+
+describe "user sees other locations that carry that snack" do
+ it 'shows them what machines' do
+  owner = Owner.create(name: "Fun time")
+  machine = owner.machines.create(location: "Mars")
+  snack = Snack.create(name: "nerds", price: "1.00")
+
+  MachineSnack.create(snack: snack, machine: machine)
+
+  visit snack_path(snack)
+
+  expect(page).to have_content(machine.location)
+  expect(page).to have_content(machine.average_price)
+  expect(page).to have_content(machine.snacks.count)
+ end
+end
