bf83c5ab1c013f67fd91decdc628a65256447f7e
kylesallette <kylesallette@gmail.com>
2018-02-18 21:48:16 -0700

    working through test

--- spec/features/user_visits_snack_page_and_sees_name_price_spec.rb
+++ spec/features/user_visits_snack_page_and_sees_name_price_spec.rb
@@ -1,15 +1,12 @@
-describe "User visit specific machines page" do
-  it "they see a all snacks and prices" do
-    sam = Owner.create(name: "Sam's Snacks")
-    machine = sam.machines.create(location: "Don's Mixed Drinks")
-    snack_1 = machine.snacks.create(name: "snickers", price: 1.00)
-    snack_2 = machine.snacks.create(name: "nerds", price: 2.00)
+describe "User visit snack  page" do
+  it "they see snack name and price" do
+    snack_1 = Snack.create(name: "snickers", price: 1.00)
+    snack_2 = Snack.create(name: "nerds", price: 2.00)
 
-    visit machine_path(machine)
+    visit snack_path(snack_1)
 
     expect(page).to have_content(snack_1.name)
     expect(page).to have_content(snack_1.price)
-    expect(page).to have_content(snack_1.name)
-    expect(page).to have_content(snack_1.price)
+
   end
 end
